743aa689f5440ae5f8f5241f8b025864e0bce396
Арслан <arslansmirnov38@gmail.com>
2024-06-17 20:41:58 +0300

    создал файлы  7.03

---
+++ 25.05.2024/25.05.2024.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+int main()
+{
+	char string[] = "wersd xfd";
+	for (int i=0;i < string; i++) {
+		
+	}
+}
+
---
+++ 7.03/7.03.cpp
@@ -0,0 +1,7 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int arr2[] = { 123,124,125,126,127,128,129 };
+}
+
