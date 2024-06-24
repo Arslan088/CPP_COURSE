f04f5ae960a4c796c3c0e9be6fa3275e3a3057b8
Arslan <arslansmirnov382gmail.com>
2024-06-24 20:42:28 +0300

    выполнял работу 3.02

---
+++ 3.02/3.02.cpp
@@ -0,0 +1,25 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{   //1
+	int number,sum;
+	cout << "Введите число:";
+	cin >> number;
+	for (int i = 0; i < 500; i++) {
+		
+		sum = sum + number;
+	}
+	//2
+	int x, y,sum;
+	cout << "Введите x :";
+	cin >> x;
+	cout << "Введите у :";
+	cin >> y;
+	for (int i = 0; i < y; i++)
+	{
+		x = x * x;
+	}
+	//3
+	cout<<"Введите "
+}
